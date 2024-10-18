78de940cae4d2226c4a47a457a82e76565c53605
Jorge Coutinho <jorgecsn@id.uff.br>
2024-10-18 02:13:24 -0300

    Update Ruby version in asdf.sh

--- software/asdf.sh
+++ software/asdf.sh
@@ -11,8 +11,8 @@ echo 'export EDITOR="code --wait"' >> ~/.bashrc
 asdf plugin add ruby
 asdf plugin add nodejs
 
-asdf install ruby 3.3.4
-asdf global ruby 3.3.4
+asdf install ruby 3.3.0
+asdf global ruby 3.3.0
 
 asdf install nodejs 20.18.0
 asdf global nodejs 20.18.0
