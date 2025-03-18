7f74363569d8216becc5dd04dab390092e33d701
jorge coutinho <102194363+jorgecoutinhobr@users.noreply.github.com>
2025-03-18 20:10:39 -0300

    Update zellij.sh

--- software/zellij.sh
+++ software/zellij.sh
@@ -1,5 +1,5 @@
 cd /tmp
-wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
+wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/download/v0.41.0/zellij-x86_64-unknown-linux-musl.tar.gz"
 tar -xf zellij.tar.gz zellij
 sudo install zellij /usr/local/bin
 rm zellij.tar.gz zellij
